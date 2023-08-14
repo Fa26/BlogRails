5296380a8f6ca0f995744bd24223fac51029929c
Fa26 <faaaa26@gmail.com>
2023-08-14 17:40:06 -0600

    de nuevo

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'pages/about'
+
+  # Defines the root path route ("/")
+   root "posts#index"
+end
